c02fd944bad56ea0286fc17e3773ac735ea51f2f
aayusht200 <aayush.t200@gmail.com>
2025-07-22 20:00:44 -0400

    done with js logic

--- calculator.js
+++ calculator.js
@@ -1,8 +1,11 @@
 const number = document.getElementsByClassName("number");
 let textArea = document.getElementById("result");
 const clearFunction = document.getElementsByClassName("calc");
-const evalFunction = document.getElementsByClassName("function");
-
+const operatorFunction = document.getElementsByClassName("function");
+let numberOne = null;
+let numberTwo = null;
+let operator = "";
+let result = null;
 textArea.value = null;
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", (e) => {
@@ -20,24 +23,62 @@ for (let i = 0; i < clearFunction.length; i++) {
         textArea.value = String(Number(textArea.value) * -1);
     } else {
       textArea.value = null;
+      numberOne = null;
+      numberTwo = null;
     }
   });
 }
 
-function calculator(eval, numberOne, numberTwo) {
-  if (eval == "+") {
+function calculator(operator, numberOne, numberTwo) {
+  if (operator == "+") {
     return numberOne + numberTwo;
-  } else if (eval == "-") {
+  } else if (operator == "-") {
     return numberOne - numberTwo;
-  } else if (eval == "/") {
+  } else if (operator == "/") {
     if (numberTwo != 0) {
       return numberOne / numberTwo;
     } else {
       return "infinity";
     }
-  } else if (eval == "%") {
+  } else if (operator == "%") {
     return numberOne % numberTwo;
-  } else if (eval == "*") {
+  } else if (operator == "*") {
     return numberOne * numberTwo;
+  } else {
+    return false;
   }
+}
+
+for (let i = 0; i < operatorFunction.length; i++) {
+  operatorFunction[i].addEventListener("click", (e) => {
+    if (numberOne == null) {
+      numberOne = Number(textArea.value);
+      textArea.value = null;
+      if (operatorFunction[i].textContent != "=")
+        operator = operatorFunction[i].textContent;
+      else {
+        textArea.value = numberOne;
+        numberOne = null;
+      }
+    } else {
+      numberTwo = Number(textArea.value);
+      if (operatorFunction[i].textContent == "=" && numberTwo != null) {
+        result = calculator(operator, numberOne, numberTwo);
+        textArea.value = result;
+        numberOne = result;
+        numberTwo = null;
+        result = null;
+      } else {
+        result = calculator(operator, numberOne, numberTwo);
+        textArea.value = result;
+        numberOne = result;
+        numberTwo = null;
+        result = null;
+        operator = operatorFunction[i].textContent;
+      }
+    }
+    console.log(numberOne);
+    console.log(numberTwo);
+    console.log(operator);
+  });
 }
